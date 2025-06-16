343ba43bea58850eee6710198d115545b8f75818
Onos <onosejoor14@gmail.com>
2025-06-16 21:34:03 +0100

    update code, move code to keep project DRY

--- client/src/app/signin/page.tsx
+++
@@ -1,5 +0,0 @@
-import SignInForm from "./_components/signin-form"
-
-export default function SignInPage() {
-  return <SignInForm />
-}
--- client/src/lib/session/decodeJwt.ts
+++
@@ -1,8 +0,0 @@
-export function decodeJwt(token: string) {
-  const [header, payload] = token.split(".");
-  const decodedHeader = JSON.parse(atob(header));
-  const decodedPayload = JSON.parse(atob(payload));
-
-  
-  return { header: decodedHeader, payload: decodedPayload };
-}
